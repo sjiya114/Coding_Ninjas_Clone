d73553fb64dd28697d3b560c625a97d679cb00f0
sjiya114 <sjiya04072004@gmail.com>
2025-09-02 17:25:22 +0530

    edit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import {motion} from 'framer-motion'
 function Features() {
   return (
     <div className='bg-black h-full min-xl:h-screen max-md:pb-12'>
        <div className=' max-w-screen-xl leading-3.5 flex flex-col mx-80 max-md:mx-0 max-md:mr-6 max-md:ml-3 max-md:w-full  space-y-16'>
-       <p className='text-white mt-20 text-xl font-semibold'>Always available when you stuck</p>
-       <p className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>Resolve doubts any time through chat, voice notes or calling.</p>
-       <p className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>500+ dedicated Teaching Assistants to resolve your doubts quickly</p>
-       <p className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>5/5 rating for 90% doubt resolutions</p>
+       <motion.p  whileInView={{color:'yellow'}} viewport={{margin:'-500px'}} className='text-white mt-20 text-xl font-semibold'>Always available when you stuck</motion.p>
+       <motion.p  whileInView={{color:'yellow'}} viewport={{margin:'-500px'}} className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>Resolve doubts any time through chat, voice notes or calling.</motion.p>
+       <motion.p  whileInView={{color:'yellow'}} viewport={{margin:'-500px'}}  className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>500+ dedicated Teaching Assistants to resolve your doubts quickly</motion.p>
+       <motion.p  whileInView={{color:'yellow'}} viewport={{margin:'-500px'}}  className='text-zinc-800 text-5xl max-md:text-3xl font-bold'>5/5 rating for 90% doubt resolutions</motion.p>
     </div>
     </div>
   )
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { color, motion } from 'framer-motion'
 function Name() {
+   const letterAnimation = {
+  initial: { color: "gray" },
+  animate: { color: "white" },
+};
   return (
      <div className='bg-black h-[50vh] flex items-center justify-center'>
-        <h1 className='text-zinc-900 text-9xl max-md:text-8xl max-md:text-center font-bold'>Coding Ninjas</h1>
-     </div>
+       <h1 
+     className='text-zinc-950 text-shadow-2xs shadow-2xl text-9xl max-md:text-8xl max-md:text-center font-bold'>Coding Ninjas</h1>
+        <motion.span   animate={{ marginRight: ["55%", "-48%"] }}
+  transition={{
+    repeat: Infinity,
+    repeatType: "reverse",
+    duration: 2,
+    ease: "easeInOut"
+  }}
+    className="w-[40px] h-[0px]  absolute mb-[80px] rounded-full"
+    style={{
+      background: "black",
+      boxShadow: "0px 40px 60px 20px rgba(255,255,255,0.5)",
+    }}
+  ></motion.span>
+      </div>
   )
 }
 
